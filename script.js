659fd1ad5ac8e1cd07e9997e26d8198e6b4616c0
Ismail_Dev <yotech42@gmail.com>
2023-07-10 02:23:05 +0600

    Sidcup Family Golf Clone Websie

---
+++ script.js
@@ -0,0 +1,109 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor_blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + 30 + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.transform = "scale(3)";
+    crsr.style.border = "0.5px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.transform = "scale(1)";
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "transparent";
+  });
+});
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  height: "110px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "nav",
+    scroller: "body",
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 2,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#aboutus img,#about-us-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: "#aboutus",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 3,
+  },
+});
+
+gsap.from(".card", {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    start: "top 50%",
+    end: "top 45%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    start: "top 50%",
+    end: "top 45%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#page4 h1", {
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 70%",
+    delay: 0.5,
+    scrub: 1,
+  },
+});
